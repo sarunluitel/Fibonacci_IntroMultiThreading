63e6d52c3b1038d202a2a972d81952277cfcba94
Sarun <sarunluitel@gmail.com>
2017-10-24 12:17:42 -0600

    single thread implemented

--- src/main.java
+++ src/main.java
@@ -1,39 +1,82 @@
-public class main extends Thread
+class main
 {
-
-  private final String NAME = null; //Name of thread
-  private long step = 0; //Steps since start.
-  private long z;      // fib(step)
-  private long y = 1;  // fib(step-1)
-  private long x = 1;  // fib(step-2)
 
   public static void main(String[] args)
   {
-    System.out.println("hello world");
-    main main = new main();
+    main mainObj = new main();
+    mainObj.instantiate();
 
-    main.start();
-    main.start();
+  }
+  private void instantiate()
+  {
+   // Fib thread1 = new Fib("Thread 1");
+    Fib thread2 = new Fib("Thread 2");
+
+    runThreads(null,thread2);
+  }
+
+  private void runThreads(Fib threadA, Fib threadB)
+  {
+    //threadA.start();
+    threadB.start();
+
+    try
+    {
+      for (int i = 0; i < 10; i++)
+      {
+       // threadA.printInfo();
+        threadB.printInfo();
+
+        System.out.println();
+        Fib.sleep(2000);
+      }
+    }
+    catch(InterruptedException e){
+      System.out.println(e.getMessage());
+    }
+    System.exit(11);
 
   }
 
-
-  @Override
-  public void run()
+  class Fib extends Thread
   {
-    synchronized ((Long)z)
+    private final String NAME; //Name of thread
+    private long step = 0; //Steps since start.
+    private long z;      // fib(step)
+    private long y = 1;  // fib(step-1)
+    private long x = 1;  // fib(step-2)
+    private long temp= 0;
+
+    Fib(String threadName)
     {
-      while (z != 7540113804746346429L)
-      {
-        z = y + x;
-        System.out.println("Inside the Thread       "+z+"   steps   "+step);
-        x = y;
-        y = z;
-        step++;
-      }
+
+      NAME=threadName;
+
     }
 
-    System.out.println(step);
 
+    @Override
+    public void run()
+    {
+      while (true)
+      {
+
+        if (z == 7540113804746346429L){x=1;y=1;}
+
+        z = y + x;
+        temp = x;
+        x = y;
+        y = z;
+        System.out.println("From Thread "+NAME+ " Number Sequences   " +
+            temp+"   "+x +"   "+y);
+
+      }
+    }
+    void printInfo()
+    {
+      System.out.println("From Thread "+NAME+ " Number Sequences   " +
+          temp+"   "+x +"   "+y);
+
+    }
   }
 }
