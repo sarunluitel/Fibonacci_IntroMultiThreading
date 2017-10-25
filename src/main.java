3f854ce1b67f247ca70fad14db20edcda0b58c0c
Sarun <sarunluitel@gmail.com>
2017-10-24 19:53:22 -0600

    multi threads Implemented

--- src/main.java
+++ src/main.java
@@ -1,41 +1,39 @@
 class main
 {
-
   public static void main(String[] args)
   {
     main mainObj = new main();
     mainObj.instantiate();
-
   }
+
   private void instantiate()
   {
-   // Fib thread1 = new Fib("Thread 1");
+    Fib thread1 = new Fib("Thread 1");
     Fib thread2 = new Fib("Thread 2");
 
-    runThreads(null,thread2);
+    runThreads(thread1,thread2);
   }
 
   private void runThreads(Fib threadA, Fib threadB)
   {
-    //threadA.start();
+    threadA.start();
     threadB.start();
-
     try
     {
       for (int i = 0; i < 10; i++)
       {
-       // threadA.printInfo();
+        Fib.sleep(2000);
+        threadA.printInfo();
         threadB.printInfo();
 
         System.out.println();
-        Fib.sleep(2000);
+
       }
     }
     catch(InterruptedException e){
       System.out.println(e.getMessage());
     }
     System.exit(11);
-
   }
 
   class Fib extends Thread
@@ -49,34 +47,39 @@ class main
 
     Fib(String threadName)
     {
-
       NAME=threadName;
-
     }
-
 
     @Override
     public void run()
     {
+      calculate();
+    }
+    void calculate()
+    {
       while (true)
       {
-
-        if (z == 7540113804746346429L){x=1;y=1;}
-
-        z = y + x;
-        temp = x;
-        x = y;
-        y = z;
-        System.out.println("From Thread "+NAME+ " Number Sequences   " +
-            temp+"   "+x +"   "+y);
-
+        synchronized (this)
+        {
+         // 7540113804746346429L maxVal in long for fib
+          if (z == 7540113804746346429L)
+          {
+            x = 1;
+            y = 1;
+          }
+          step++;
+          z = y + x;
+          temp = x;
+          x = y;
+          y = z;
+        }
       }
     }
-    void printInfo()
+    synchronized void printInfo()
     {
-      System.out.println("From Thread "+NAME+ " Number Sequences   " +
-          temp+"   "+x +"   "+y);
 
+      System.out.println("From Thread "+NAME+" steps: " +step + " Number Sequences   " +
+          temp+"   "+x +"   "+y);
     }
   }
 }
