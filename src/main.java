5f7aace7666ef65f0d05c1513cb3e91eab0217da
Sarun <sarunluitel@gmail.com>
2017-10-24 21:58:51 -0600

    multi threads Implemented

--- src/main.java
+++ src/main.java
@@ -1,5 +1,13 @@
+/**************************************************************************
+ @author Sarun Luitel
+ This program makes two threads compute the Fibonacci series independently.
+ The main thread invokes method in each worker threads to print what they're
+ currenty computing every 2 seconds. after 10 cycles (20 cycles) it kills the
+ threads and terminates the program.
+ **************************************************************************/
 class main
 {
+  //instantiate itself to use the instance variables.
   public static void main(String[] args)
   {
     main mainObj = new main();
@@ -8,6 +16,7 @@ class main
 
   private void instantiate()
   {
+    // create object from class that extend thread.
     Fib thread1 = new Fib("Thread 1");
     Fib thread2 = new Fib("Thread 2");
 
@@ -20,20 +29,32 @@ class main
     threadB.start();
     try
     {
-      for (int i = 0; i < 10; i++)
+      int i=0;
+      for (;;)//infinite loop to check the threads
       {
+        i++;
         Fib.sleep(2000);
-        threadA.printInfo();
-        threadB.printInfo();
 
+        if (!threadA.isAlive() && !threadB.isAlive())
+        {//both threads dead
+          System.out.println("Both threads Dead");
+          System.exit(1);//terminate program
+        }
+
+        threadA.printInfo();// print current computation.
+        threadB.printInfo();
+        if (i==10)
+        {
+          threadA.alive=false;//kill threads
+          threadB.alive=false;
+          System.out.println();
+        }
         System.out.println();
+
 
       }
     }
-    catch(InterruptedException e){
-      System.out.println(e.getMessage());
-    }
-    System.exit(11);
+    catch(InterruptedException e){}
   }
 
   class Fib extends Thread
@@ -44,6 +65,7 @@ class main
     private long y = 1;  // fib(step-1)
     private long x = 1;  // fib(step-2)
     private long temp= 0;
+    volatile boolean alive=true;
 
     Fib(String threadName)
     {
@@ -51,17 +73,15 @@ class main
     }
 
     @Override
-    public void run()
-    {
-      calculate();
-    }
+    public void run() { calculate(); }
+
     void calculate()
     {
-      while (true)
+      for (;;)//infinite loop to do calculation
       {
         synchronized (this)
         {
-         // 7540113804746346429L maxVal in long for fib
+          // 7540113804746346429L maxVal in long for fib
           if (z == 7540113804746346429L)
           {
             x = 1;
@@ -73,12 +93,16 @@ class main
           x = y;
           y = z;
         }
+        if(!alive) // check if alive every cycle
+        {
+          System.out.println(NAME + " Dies on step: "+step);
+          return;
+        }
       }
     }
-    synchronized void printInfo()
+    synchronized void printInfo() //synchronized method to print details
     {
-
-      System.out.println("From Thread "+NAME+" steps: " +step + " Number Sequences   " +
+      System.out.println("From Thread "+NAME+" steps: " +step + " | Number Sequences:   " +
           temp+"   "+x +"   "+y);
     }
   }
